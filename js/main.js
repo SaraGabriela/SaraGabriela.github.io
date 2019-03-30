4c2ebae0ecf8a1e71e815b48a13a4f674d9394e8
Maday <maday.choque@gmail.com>
2019-03-30 17:16:04 -0500

    4

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-var imgRoleModelData = ['INÉS MELCHOR','HEDY LAMAR','ELENA CAFFARENA','MARÍA ELENA MOYANO','LUCHA REYES'];
+var imgRoleModelData = ['SOFÍA MULANOVICH','MARÍA ELENA MOYANO','LUCHA REYES','INÉS MELCHOR','HEDY LAMAR','ELENA CAFFARENA'];
 var descriptionRoleModelData = ['El servicio gratuito de Google traduce instantáneamente palabras, frases y páginas web del español a más de 100 idiomas y viceversa.',
                             '111111111111111111111111 traduce instantáneamente palabras, frases y páginas web del español a más de 100 idiomas y viceversa.',
                             '222222222222222 El servicio gratuito de Google traduce instantáneamente palabras, frases y páginas web del español a más de 100 idiomas y viceversa.'
@@ -7,12 +7,17 @@ var selector=0;
 var path = "image/";
 var imgFormat = '.png';
 function render(){    
-    var imgRoleModel = document.getElementsByClassName('role-model');
-    imgRoleModel[0].src = path+imgRoleModelData[selector]+imgFormat;
-    imgRoleModel[1].src = path+imgRoleModelData[selector+1]+imgFormat;
-    imgRoleModel[2].src = path+imgRoleModelData[selector+2]+imgFormat;   
-    console.log(path+imgRoleModelData[selector]);     
+    var imgRoleModel = [document.getElementById('role-model-behind'), document.getElementById('role-model-selected'),document.getElementById('role-model-fordward')];
+    var src1 = path+imgRoleModelData[selector]+imgFormat;
+    var src2 = path+imgRoleModelData[selector+1]+imgFormat;
+    var src3 = path+imgRoleModelData[selector+2]+imgFormat; 
+    imgRoleModel[0].src = src1;
+    imgRoleModel[1].src = src2;
+    imgRoleModel[2].src = src3;      
     var descriptionRoleModelP = document.getElementById('role-model-description');
     descriptionRoleModelP.innerHTML=descriptionRoleModelData[selector];
     selector++;
+    if(selector>3){
+        selector=0;
+    }
 }
